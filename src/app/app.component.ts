eda4c68d1c25ddee8acad6595a4ce6e4530b73ba
Juan Camilo Gaspar Arias <camilo.gaspar@payulatam.com>
2020-05-01 03:21:21 -0500

    Home finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'about-me';
+
+  toggleActive: boolean = false;
+
+  toggle() {
+    this.toggleActive = !this.toggleActive
+  }
 }
